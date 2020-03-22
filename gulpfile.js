05cb35f02465ceee3413c5f3a41bad693fac4a92
ammarfalmban <devAspirant@gmail.com>
2020-03-22 18:44:17 +0300

    auto refresh

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('default',['build','connect','watch']); // run 
+
+// بناء السيرفر 
+gulp.task('connect',function(){
+    connect.server({
+        root: 'build',
+        livereload: true,
+        port: 8080,
+    })
+});
 
 gulp.task('watch',  function(){
     return gulp.watch("src/**/*",["build"]);
@@ -6,22 +18,29 @@ gulp.task('watch',  function(){
 
 gulp.task("html", function(){
     return gulp.src("src/*.html")
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build"))
+    .pipe(connect.reload()); /* connect to the sever */ 
+    
 });
 
 gulp.task("css", function(){
     return gulp.src("src/css/*.css")
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build"))
+    .pipe(connect.reload() /* connect to the sever */ );
 });
 
 gulp.task("js", function(){
     return gulp.src("src/js/*.js")
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build"))
+    .pipe(connect.reload() /* connect to the sever */ 
+    );
 });
 
 gulp.task("images", function(){
     return gulp.src("images/*")
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build"))
+    .pipe(connect.reload() /* connect to the sever */ 
+    );
 });
 
 gulp.task("build",["html","css","js","images"]);
