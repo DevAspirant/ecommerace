364604aa45389654b093aaedd6e612b797f11783
ammarfalmban <devAspirant@gmail.com>
2020-03-22 12:55:56 +0300

    modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
 
-gulp.task('watch', async function(){
+gulp.task('watch',  function(){
     console.log(" Hello Gulp");
+});
+
+gulp.task("build", function(){
+    return gulp.src("src/*.html")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(){
+    return gulp.src("src/css/*.css")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(){
+    return gulp.src("src/js/*.js")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(){
+    return gulp.src("images/*")
+    .pipe(gulp.dest("build"));
 });
